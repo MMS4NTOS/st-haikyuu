4ed0a52541bac9d9df0929ba886e3a52ef9ae3ba
MMS4NTOS <101435325+MMS4NTOS@users.noreply.github.com>
2022-11-11 11:21:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,232 @@
+let carta1 = {
+    nome:"Karasuno",
+    img: "https://i.pinimg.com/236x/cc/17/c2/cc17c27e8fe10b460879fd73028ce2c5--haikyuu-karasuno-volleyball.jpg",
+    atributos: {
+      Ataque : 9,
+      Bloqueio : 7,
+      Saque : 7,
+      Recepção : 6,
+      Levantamento : 9,
+        
+    }  
+  }
+  
+  let carta2 = {
+    nome:"Shiratorizawa",
+    img: "https://i.pinimg.com/originals/bc/ec/b6/bcecb6535dacf8bd80077240a4313c3f.jpg",
+    atributos: {
+      Ataque : 10,
+      Bloqueio : 8,
+      Saque : 8,
+      Recepção : 7,
+      Levantamento : 8,
+        
+    }  
+  }
+  
+  let carta3 = {
+    nome:"Aoba Johsai",
+    img: "http://pm1.narvii.com/6441/66a132e0ec32b8fd67760e7cdcdd4a1f826cc558_00.jpg",
+    atributos: {
+      Ataque : 8,
+      Bloqueio : 7,
+      Saque : 9,
+      Recepção : 8 ,
+      Levantamento : 10,
+      
+    }  
+  }
+  
+  let carta4 = {
+    nome:"Nekoma",
+    img: "https://ae01.alicdn.com/kf/H08110220ff604d14bf90e9cea27f24c7h.jpg",
+    atributos: {
+      Ataque : 8,
+      Bloqueio : 9,
+      Saque : 7,
+      Recepção : 10 ,
+      Levantamento : 8,
+       
+    }  
+  }
+  
+  let carta5 = {
+    nome:"Dateko",
+    img: "https://i.pinimg.com/originals/33/c5/2b/33c52b23f91ecb459fbb7df20755d687.jpg",
+    atributos: {
+      Ataque : 8,
+      Bloqueio : 10,
+      Saque : 7,
+      Recepção : 8 ,
+      Levantamento : 7,
+       
+    }  
+  }
+  
+  let carta6 = {
+    nome:"Inarizaki",
+    img: "https://i.pinimg.com/originals/71/3c/41/713c41209b78227a320e782c4cfc65a9.jpg",
+    atributos: {
+      Ataque : 8,
+      Bloqueio : 8,
+      Saque : 8,
+      Recepção : 7 ,
+      Levantamento : 9,
+      
+    }  
+  }
+  
+  let carta7 = {
+    nome:"Fukurodani",
+    img: "http://pm1.narvii.com/7058/4086434060e6b1aec11862eaeb47186a4e0bb074r1-500-707v2_00.jpg",
+    atributos: {
+      Ataque : 9,
+      Bloqueio : 9,
+      Saque : 7,
+      Recepção : 6 ,
+      Levantamento : 7,
+    }  
+  }
+  let carta8 = {
+    nome:"Johzenji",
+    img: "https://quality-of-life-ryoblog.com/wp-content/uploads/2021/09/f99e8b2f39c8f854be50ced85b0f436d-e1632918826295.jpg",
+    atributos: {
+      Ataque : parseInt(Math.random() * 11),
+      Bloqueio : parseInt(Math.random() * 11),
+      Saque : parseInt(Math.random() * 11),
+      Recepção : parseInt(Math.random() * 11) ,
+      Levantamento : parseInt(Math.random() * 11),
+    }  
+  }
+  let carta9 = {
+    nome:"Kakugawa",
+    img: "https://lh3.googleusercontent.com/brc5xnkdSBDWzfZmxSPXn8dmbZLVDYFCg7F4WfrkhHT49GEPJr8wzEl35uzMHbMd88tMq7ZitMs8UIGllmaGWiX9T0Lzx21fW0lu6dwEhsQTPbo-sEHVT1Is5TEhsAkQpjicTHWcJE7hp3S8-iXi7GRa2OYOfYwQoLXNYc0W0CWVrJ-5L0ldUL30S81yj0l7ZA2VC0u8MDK9qE87_AFuxJUT4_XSJM50iHdp8Vtmw5ao6aywxX9yRRiw4eIWgx2II8cAxHVVrEuKGBJ_buzACNiGm8w5r246uHbDGR_0dGHV6fOzMnGRvM3DNV3OVRHV-G-KbgUv2uCpXnoHiMLtEcMIk15DcWFLt-VW4lGY3kERbhWcK1nEYqv2ORKovHR9H2zapU7NKIc5a8oxKaDKIY40_-8KtLruiKbYXxHqee5n2Os-XXBvnc_e6LtPERW0MOwtXYdr_4ZV9MIimre-6bExzX45uuSXvQ5k6_K7vERRl5qKRVX5DGjd2ABbVz1jvkEdmZa37eKBY5W77DHAIAS_NICmBPETiZc5Gn0py7YRWnvZjzUk77PxTV-_eL2mc8mqiGRTdnd0iQ1v3vSkQkK5K-lH4sV-P1j-ev2QBn3jDrDsrkcZ6dWbx2SBwOqbsKSrbCx8EPojQI5_cEB2k8OPXEDc0YLiFZrz8GAaWHHaFPZW5hrRIV5YUBjdYZjPhddlbaP6G97gQcJ-uAFNhZx2=w338-h220-no?authuser=0",
+    atributos: {
+      Ataque : 6,
+      Bloqueio : 9,
+      Saque : 6,
+      Recepção : 5 ,
+      Levantamento : 6,
+    }  
+  }
+  let carta10 = {
+    nome:"Time Nacional do Japão",
+    img: "https://lh3.googleusercontent.com/kfHRaxmWN2fNzeUr9TxjFM6U-Jg6mGKac0w8Zzx3Y6tjEqpev7huxcv_RgOtkGqlOVImCOrrG-GwErfsBj-JJNyF7Gptt7eir686Sw84oMkl0YI3LQNAOvVn6kGVkN_64H09ZY_Jz17TnSernOq0wmyRWU98-QSWA2toSAeyKhi1-y4tQr9ZcjBXt6hqPYBj8SB8Xa55hKyhNsspJsWbIG_43e1MXCAmxwtvgWCq9vTMG-RMkhXScTGhQKj97x6Y90HR4JX1tMapczUrAQM1Q6ua-PYRMiDra-Mn9fI6Z-Pll3DdzLRVrPhtWBYgR7P5-SBtI1iPBVta1rHNBVBsJaAcbqYX_4OVWqRsidvMT2Xou8-476MEBeRvGm3Rv5jH4zi21pcnYSFZKUlPxd9qXqUHrkiIxMLPHgbb5iVA1p67Qi5WJtmWoxmxAmMdoyuILSMgHHloko4zKL9P_dKquyT8n8JWyGEfr_dI6w4wlPxQbvwZMwMYb29RewGKm0gTRE9E-QlmsT89f9AIluPx8bKZp4ArKnSur7eJxqjXUrEYBjsQNLcUmJuwlSJPWy0j-KWtmuCbYIr34T2rSrVQ03C7uafVEl1RPKKuAP3ZZIZcia6v-5HeisTJ7a7DyAsFmsEEyIMrGI-aU31n_ShSkK1ABpH5mtN1rHjnuYzFz1CgRYtL6e8HvD3Efjtyo8hNgf1IUTq7-Evh9fYTCEQxV1Q5=w314-h462-no?authuser=0",
+    atributos: {
+      Ataque : 10,
+      Bloqueio : 10,
+      Saque : 10,
+      Recepção : 10 ,
+      Levantamento : 10,
+    }  
+  }
+  let carta11 = {
+    nome:"Wakutani",
+    img: "https://lh3.googleusercontent.com/WDVJeagVZ2k0-g6JUyPisNMzlDLsXX-UpTl5oCxFI67Y6lGbgPEbajo1a54tLiYMx8RTvtJ3vOTQkn35yiGCXRKGT5k_1P53_Jmm8qjMp7poG1lfxQ58NVY1ikO3iXZNM9hDloZuk43TZJeJMqci1nL7QChgNkyaX5N82l5sAWK2vO_5fKOzUDVM7eeF6CTy-KkEaAS5tngV4yVtKNvmvLsWGHrlnXXxlqJv69_oCbHDdCWjBv98lOkLfjKBI2wZCu__C_FK4GsD85Xkiuzp6PbY_v8msDX3rjCRkgfzSaHHJNK0C2IO5IXeS0rbRc0rTBVoxxH4604iY6YlF4-VrzLbmbnydCzQBX6CCCAdfpb3oOM4tvD_z2T35gqeNE3wljIcxpjUem8z_Ll1uAgJDxEg8R57EwM6-W5NoJAVJyNSR1kahjSezAkculgQgqHZ-V52kkkGA-sidWahhoXgqb6FfU4HZowk-0sxrWndDus-m1y-XK69jWH08YbM8DR5Rlh7jgGK3CU1iuIwveFDu-JOmM1eYuXt_0wka35yiw0aX0eEZiFd_R79CzzwEsBcnV60rytj0kZgsCcmorAT20Lkh5MEgwoY9k2aD1k6Xy1MI2ma_ywlQfkto5gmeXebQGDmgunwl-bcSDzc6IT8ziqnnZaW5T8u2HYlVtjDZiO_BsoWtVBNZYy_KzscVxZ4JdiKwiVRnVaM4utCqACc2TiQ=w1168-h657-no?authuser=0",
+    atributos: {
+      Ataque : 7,
+      Bloqueio : 7,
+      Saque : 6,
+      Recepção : 8 ,
+      Levantamento :6,
+    }  
+  }
+  let carta12 = {
+    nome:"Kamomedai",
+    img: "https://i-ogp.pximg.net/c/540x540_70/img-master/img/2019/11/16/19/43/10/77844393_p0_square1200.jpg",
+    atributos: {
+      Ataque : 10,
+      Bloqueio : 8,
+      Saque : 8,
+      Recepção : 9 ,
+      Levantamento : 8,
+    }  
+  }
+  
+  
+  let baralho = [carta6, carta5, carta4, carta3,  carta2, carta1, carta12, carta11, carta10, carta9, carta8, carta7]
+  let cartaNPC
+  let cartaPlayer
+  
+  function sortearCarta() {
+  let numeroCartaNPC = parseInt(Math.random() * 11)
+     cartaNPC = baralho[numeroCartaNPC]
+    
+  let numeroPlayer = parseInt(Math.random() * 11) 
+  while (numeroPlayer == numeroCartaNPC) {
+    numeroPlayer = parseInt(Math.random() * 11)
+  }
+    cartaPlayer = baralho[numeroPlayer]
+  
+    
+    document.getElementById("btnSortear").disabled=true;
+    document.getElementById("btnJogar").disabled=false;
+    console.log(cartaPlayer)
+   
+    exibirJogador()
+  }
+   
+  function exibirJogador(){
+   let divCartaJogador = document.getElementById("carta-jogador")
+    let tagHTML = "<div id='opcoes' class='carta-status'>"
+    let opcoesTexto = "" 
+      for (var atributos in cartaPlayer.atributos) {
+    opcoesTexto += "<input type='radio'  name='atributo' value=" + atributos + ">" 
+    + atributos +  " " + cartaPlayer.atributos[atributos] + " <br> </input>";
+    
+    }
+    let nome = `<p id="nomeJogador" class="carta-subtitle">${cartaPlayer.nome}</p> `;
+  divCartaJogador.innerHTML =  "<img class='card' src="+ cartaPlayer.img +
+   " width='460' height='350'>" + nome + tagHTML + opcoesTexto + "</div>";
+    
+  }
+  
+  function exibirNPC(){
+  let divCartaNPC = document.getElementById("carta-maquina")
+  
+    let tagHTML = "<div id='opcoes2' class='carta-status'>"
+    let opcoesTexto = "" 
+      for (var atributos in cartaNPC.atributos) {
+    opcoesTexto += "<p  id='resultado' name='atributo' value=" + atributos + ">" + atributos +  " " + cartaNPC.atributos[atributos] + " </p>";
+    }
+    let nome = `<p id="nomeNPC" class="carta-subtitle">${cartaNPC.nome}</p> `;
+  divCartaNPC.innerHTML =  "<img class='card' src="+ cartaNPC.img + " width='460' height='350'>" + nome + tagHTML + opcoesTexto + "</div>";
+  }
+
+  function jogar() {
+    let atributoSelecionado = obtemAtributo()
+    let valorCartaPlayer = cartaPlayer.atributos[atributoSelecionado]
+    let valorCartaNPC = cartaNPC.atributos[atributoSelecionado]
+    let elementoResultado = document.getElementById("resultado")
+      if (valorCartaPlayer > valorCartaNPC) {
+        elementoResultado.innerHTML = "Parabéns, você venceu o jogo! O time adversário era o " + cartaNPC.nome  + ", e seu atributo " + atributoSelecionado + " era de " + valorCartaNPC
+         exibirNPC()
+        }
+      else if (valorCartaNPC >= valorCartaPlayer) {
+        elementoResultado.innerHTML = "Dessa vez você perdeu! Quem sabe no próximo set? O Time adversário era " + cartaNPC.nome + ", e seu atributo " + atributoSelecionado + " era de " + valorCartaNPC
+        ;
+         exibirNPC()
+    }
+
+    function obtemAtributo() {
+      let radioAtributo = document.getElementsByName("atributo")
+      
+      for (i=0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked == true)
+          return radioAtributo[i].value
+      }
+      
+    }
+     
+    }
+     function Reiniciar() {
+       let divCartaJogador = document.getElementById("carta-jogador")
+       divCartaJogador.innerHTML = ""
+       let divCartaMaquina = document.getElementById("carta-maquina")
+       divCartaMaquina.innerHTML = ""
+       let divResultado = document.getElementById("resultado")
+       divResultado.innerHTML = ""
+      document.getElementById("btnSortear").disabled=false;
+      document.getElementById("btnJogar").disabled=true;
+    }
+   
